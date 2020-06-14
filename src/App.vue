04f2f03a10a4e64084c0907a7e303b7bb759846f
Daniela <dinving@gmail.com>
2020-06-14 20:51:16 +0200

    vedlejsi vetev

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,106 @@
 <template>
   <div>
-    <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <img class="joke__user" src="assets/img/user01.png" />
-          <p
-            class="joke__text"
-          >The secret service isn't allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"</p>
-        </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up"></button>
-          <span id="likes-up" class="likes-count likes-count--up">0</span>
-          <button id="btn-down" class="btn-like btn-like--down"></button>
-          <span id="likes-down" class="likes-count likes-count--down">0</span>
-        </div>
-      </div>
-    </div>
+    <MujJokes
+      v-for="(vtip, index) in mojeVtipy"
+      v-bind:vtip="vtip.vtip"
+      v-bind:like="vtip.like"
+      v-bind:unlike="vtip.unlike"
+      v-on:likeJedna="likeplusplus"
+      v-on:unlikeJedna="unlikeplusplus"
+      v-bind:key="index"
+    />
   </div>
 </template>
 
 <script>
+import Jokes from "./components/Jokes.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    MujJokes: Jokes
+  },
+  methods: {
+    likeplusplus(index) {
+      this.mojeVtipy[index].like++;
+    },
+    unlikeplusplus(ktery) {
+      this.mojeVtipy[ktery].unlike++;
+    }
+  },
+  data() {
+    return {
+      mojeVtipy: [
+        {
+          user: 1,
+          vtip:
+            "The secret service isn't allowed to yell 'Get down!' anymore when the president is about to be attacked. Now they have to yell 'Donald, duck!'",
+          like: 13,
+          unlike: 24
+        },
+        {
+          user: 2,
+          vtip:
+            "Why did the invisible man turn down the job offer? He couldn't see himself doing it.",
+          like: 50,
+          unlike: 10
+        },
+        {
+          user: 3,
+          vtip:
+            "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+          like: 30,
+          unlike: 2
+        },
+        {
+          user: 4,
+          vtip:
+            "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+          like: 15,
+          unlike: 8
+        },
 
-  data() {}
+        {
+          user: 5,
+          vtip:
+            "My daughter screeched, 'Daaaaaad, you haven't listened to one word I've said, have you!?' What a strange way to start a conversation with me...",
+          like: 90,
+          unlike: 44
+        },
+        {
+          user: 6,
+          vtip:
+            "My wife tried to unlatch our daughter's car seat with one hand and said, 'How do one armed mothers do it?' Without missing a beat I replied, 'Single handedly.'",
+          like: 38,
+          unlike: 1
+        },
+        {
+          user: 7,
+          vtip:
+            "When a dad drives past a graveyard: Did you know that's a popular cemetery? Yep, people are just dying to get in there!",
+          like: 66,
+          unlike: 35
+        },
+        {
+          user: 8,
+          vtip:
+            "My friend keeps saying 'cheer up man it could be worse, you could be stuck underground in a hole full of water.' I know he means well.",
+          like: 27,
+          unlike: 18
+        },
+        {
+          user: 9,
+          vtip:
+            "The fattest knight at King Arthur’s round table was Sir Cumference. He acquired his size from too much pi.",
+          like: 42,
+          unlike: 9
+        }
+      ]
+    };
+  }
 };
 </script>
 <style>
 html {
   font-family: sans-serif;
-}
-
-.container {
-  max-width: 30rem;
-  margin: 5rem auto;
-}
-
-.joke {
-  margin-bottom: 2rem;
-}
-
-.joke__body {
-  display: flex;
-}
-
-.joke__user {
-  flex: 0 0 auto;
-  width: 5rem;
-  height: 5rem;
-  margin-right: 1rem;
-}
-
-.joke__text {
-  background-color: cornflowerblue;
-  color: white;
-  border-radius: 1rem;
-  padding: 1.5rem 2rem;
-  margin: 0;
-  line-height: 150%;
-}
-
-.joke__likes {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  margin-top: 1rem;
-}
-
-.likes-count {
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin: 0 0.5rem;
-}
-
-.likes-count--up {
-  color: #00e268;
-}
-
-.likes-count--down {
-  color: #ff5654;
-}
-
-.btn-like {
-  border: none;
-  outline: none;
-  width: 2rem;
-  height: 2rem;
-  border-radius: 1rem;
-  background-size: 1.2rem;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-.btn-like--up {
-  background-color: #00e268;
-  background-image: url("img/like-up.png");
-}
-
-.btn-like--down {
-  background-color: #ff5654;
-  background-image: url("img/like-down.png");
 }
 </style>
---
+++ src/components/Jokes.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container">
+    <div class="joke">
+      <div class="joke__body">
+        <img class="joke__user" v-bind:src="`/src/components/assets/users0${index}.png`" />
+        <p class="joke__text">{{ vtip }}</p>
+      </div>
+      <div class="joke__likes">
+        <button id="btn-up" class="btn-like btn-like--up" v-on:click="likeplus"></button>
+        <span id="likes-up" class="likes-count likes-count--up">{{like}}</span>
+        <button id="btn-down" class="btn-like btn-like--down" v-on:click="unlikeplus"></button>
+        <span id="likes-down" class="likes-count likes-count--down">{{unlike}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jokes",
+  props: {
+    vtip: String,
+    like: Number,
+    unlike: Number
+  },
+  data() {
+    return {
+      index: 1
+    };
+  },
+  methods: {
+    likeplus() {
+      this.$emit("likeJedna", this.like++);
+    },
+    unlikeplus() {
+      this.$emit("unlikeJedna", this.unlike++);
+    }
+  }
+};
+</script>
+<style>
+html {
+  font-family: sans-serif;
+}
+
+.container {
+  max-width: 30rem;
+  margin: 5rem auto;
+}
+
+.joke {
+  margin-bottom: 2rem;
+}
+
+.joke__body {
+  display: flex;
+}
+
+.joke__user {
+  flex: 0 0 auto;
+  width: 5rem;
+  height: 5rem;
+  margin-right: 1rem;
+}
+
+.joke__text {
+  background-color: cornflowerblue;
+  color: white;
+  border-radius: 1rem;
+  padding: 1.5rem 2rem;
+  margin: 0;
+  line-height: 150%;
+}
+
+.joke__likes {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+.likes-count {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 0.5rem;
+}
+
+.likes-count--up {
+  color: #00e268;
+}
+
+.likes-count--down {
+  color: #ff5654;
+}
+
+.btn-like {
+  border: none;
+  outline: none;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+  background-size: 1.2rem;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.btn-like--up {
+  background-color: #00e268;
+  background-image: url("/src/components//img/like-up.png");
+}
+
+.btn-like--down {
+  background-color: #ff5654;
+  background-image: url("/src/components//img/like-down.png");
+}
+</style>
